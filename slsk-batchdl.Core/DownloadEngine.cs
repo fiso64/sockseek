c875fab4c6f71b3127050cc0bddeef71ebb5fc04
fiso64 <sokfilix@gmail.com>
2026-05-08 22:45:59 +0200

    Fix mock aggregate defaults

--- slsk-batchdl.Core/DownloadEngine.cs
+++ slsk-batchdl.Core/DownloadEngine.cs
@@ -676,7 +676,7 @@ public class DownloadEngine
                 }
                 else
                 {
-                    job.SetDone();
+                    job.Fail(FailureReason.NoSuitableFileFound);
                 }
                 return;
             }
